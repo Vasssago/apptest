f97a993a43cbd016acd1f7da472f3b40198491ec
Vasssago <nurkanattrajsov@outlook.com>
2024-01-22 17:36:16 +0500

    Added MaterialUI, Navbar Component, page routing

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div>
+			<h1>000</h1>
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/pages/signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUp = () => {
+	return (
+		<div>
+			<h1>000</h1>
+		</div>
+	);
+};
+
+export default SignUp;
